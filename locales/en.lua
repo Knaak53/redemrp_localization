40df3d4c386ec01c352cff69e1396413167e2d6c
Knaak53 <manugara53@gmail.com>
2019-12-14 23:45:56 +0100

    add redemp_infection es/en locales

--- locales/en.lua
+++ locales/en.lua
@@ -2,5 +2,8 @@ Config.Locales["en"] = {
     ["redemrp_respawn"] ={
         ["LocaleDead"] = "You died!",
         ["LocaleTimer"] = "Respawning in"
+    },
+    ["redemrp_infection"] ={
+        ["msg"] = "You are getting sick..."
     }
 }
--- locales/es.lua
+++ locales/es.lua
@@ -2,5 +2,8 @@ Config.Locales["es"] = {
     ["redemrp_respawn"] ={
         ["LocaleDead"] = "Has muerto!",
         ["LocaleTimer"] = "Reaparecerás en"
+    },
+    ["redemrp_infection"] ={
+        ["msg"] = "Estás poniéndote enfermo..."
     }
 }
