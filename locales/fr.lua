6794dc4a6d94ec36671687a949589267db3dbec9
ChainTastic <kevsimon26@hotmail.fr>
2019-12-14 23:57:51 -0500

    Create fr.lua
    Baguette translation 👍 🥖

---
+++ locales/fr.lua
@@ -0,0 +1,9 @@
+Config.Locales["fr"] = {
+    ["redemrp_respawn"] ={
+        ["LocaleDead"] = "Tu es mort !",
+        ["LocaleTimer"] = "Réanimation possible dans"
+    },
+    ["redemrp_infection"] ={
+        ["msg"] = "Vous tombez malade..."
+    }
+}
